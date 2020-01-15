9a80eb15e7b5b7376d9ae92ab90f250bd15456ce
elnao <sgrb7yfe@gmail.com>
2020-01-14 21:51:47 -0600

    Updated comments

--- Bitfit-Create-Evidence-Hashes.ps1
+++ Bitfit-Create-Evidence-Hashes.ps1
@@ -1,6 +1,6 @@
 # Bitfit-Create-Evidence-Hashes.ps1
 # https://github.com/elnao/Forensics
-# Copy this file to the root of your evidence directory.  Run after you have colleccted all of you evidence.  
+# Copy this file and bitfit.exe to the root of your evidence directory.  Run after you have colleccted all of you evidence.  
 #  All files will be hashed and the results copied to a file named VERSION-Evidence-yyyyMMddThhmmssZ.txt
 
 .\bitfit.exe .\ > VERSION-Evidence-$(((get-date).ToUniversalTime()).ToString("yyyyMMddThhmmssZ")).txt
