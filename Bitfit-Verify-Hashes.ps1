3e30eb65d487cc0f33f30b2f29adf0a0986d5694
elnao <sgrb7yfe@gmail.com>
2020-01-14 21:51:12 -0600

    Updated Comments

--- Bitfit-Verify-Hashes.ps1
+++ Bitfit-Verify-Hashes.ps1
@@ -1,2 +1,7 @@
-﻿.\bitfit.exe -v .\
+# Bitfit-Verify-Hashes.ps1
+# https://github.com/elnao/Forensics
+# Copy this file and bitfit.exe to the root of the directory that you want to verify hashes
+#  All files will be hashed and the results copied to a file named VERSION-yyyyMMddThhmmssZ.txt
+
+.\bitfit.exe -v .\
 pause
