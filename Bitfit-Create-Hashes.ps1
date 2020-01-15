21a827d4bc8847307aaa1c3aabcd01b8e94c1da5
elnao <sgrb7yfe@gmail.com>
2020-01-14 21:48:24 -0600

    Added comments

--- Bitfit-Create-Hashes.ps1
+++ Bitfit-Create-Hashes.ps1
@@ -1,2 +1,7 @@
-﻿.\bitfit.exe .\ > VERSION-$(((get-date).ToUniversalTime()).ToString("yyyyMMddThhmmssZ")).txt
+# Bitfit-Create-Hashes.ps1
+# https://github.com/elnao/Forensics
+# Copy this file to the root of your forensic tools directory.  Run after you have assembled all of your forensic tools. 
+#  All files will be hashed and the results copied to a file named VERSION-yyyyMMddThhmmssZ.txt
+
+.\bitfit.exe .\ > VERSION-$(((get-date).ToUniversalTime()).ToString("yyyyMMddThhmmssZ")).txt
 pause
